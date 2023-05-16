12edbfe70455e2e5eb624a0b711ae57706494235
Daniel Pires <dfpires@gmail.com>
2023-05-16 11:00:03 -0300

    Add files via upload

--- loja/frontend/scripts.js
+++ loja/frontend/scripts.js
@@ -15,10 +15,12 @@ async function remover(id) {
     }
 
 }
-function editar(name, description, quantity){
+function editar(id, name, description, quantity){
+    document.getElementById("id").value = id
     document.getElementById("name").value = name
     document.getElementById("description").value = description
     document.getElementById("quantity").value = quantity
+
 }
 
 async function consulta(){
@@ -28,28 +30,42 @@ async function consulta(){
         })
     let conteudo = ""
     produtos.forEach(produto => {
-        conteudo += `<tr> <td> ${produto.name} </td> <td> ${produto.description} </td> <td> ${produto.quantity} </td> <td> ${produto.created_at}</td> <td> <i onClick="remover('${produto.id}')" class="bi bi-trash"> </td> <td> <i onClick="editar('${produto.name}', '${produto.description}', ${produto.quantity})" class="bi bi-pencil"/> </td> </tr>`
+        conteudo += `<tr> <td> ${produto.name} </td> <td> ${produto.description} </td> <td> ${produto.quantity} </td> <td> ${produto.created_at}</td> <td> <i onClick="remover('${produto.id}')" class="bi bi-trash"> </td> <td> <i onClick="editar('${produto.id}', '${produto.name}', '${produto.description}', ${produto.quantity})" class="bi bi-pencil"/> </td> </tr>`
     })
     // envia os dados para o HTML
     document.getElementById("tabela").innerHTML = conteudo
 }
 
-async function cadastra(){
+async function confirmar(){
+    let id = document.getElementById("id").value
     let name = document.getElementById("name").value
     let description = document.getElementById("description").value
     let quantity = Number(document.getElementById("quantity").value)
 
-    let produto = {name, description, quantity}
+    if (!name || !description || !quantity){
+        alert('Informe todos os campos')
+        return // sai da função
+    }
+    let method
+    let produto
+    if (!id){
+        method = 'POST'
+        produto = {name, description, quantity}
+    }
+    else {
+        method = 'PUT'
+        produto = {id, name, description, quantity}
+    }
 
     await fetch('http://localhost:3333/product', { // cadastra no BD
-        method: 'POST',
+        method: method,
         body: JSON.stringify(produto), // converte objeto JSON em string
         headers: {
             "Content-Type": "application/json; charset=UTF-8"
         }
     })
-    .then(resposta => alert('Inserção realizada com sucesso'))
-    .catch(erro => alert('Problema na inserção'))
+    .then(resposta => alert('Operação realizada com sucesso'))
+    .catch(erro => alert('Problema na operação'))
     // chama a função consulta
     consulta()
     // limpa os campos
